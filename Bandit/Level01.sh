328f7e6bc7658b6a8630af61d086c54665c61604
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-04 03:04:26 +0530

    Update Level01.sh

--- Bandit/Level01.sh
+++ Bandit/Level01.sh
@@ -1,1 +1,13 @@
 #Gettinf Started
+
+bandit1@bandit:~$ ls -alps
+total 24
+4 -rw-r-----  1 bandit2 bandit1   33 May  7  2020 -
+4 drwxr-xr-x  2 root    root    4096 May  7  2020 ./
+4 drwxr-xr-x 41 root    root    4096 May  7  2020 ../
+4 -rw-r--r--  1 root    root     220 May 15  2017 .bash_logout
+4 -rw-r--r--  1 root    root    3526 May 15  2017 .bashrc
+4 -rw-r--r--  1 root    root     675 May 15  2017 .profile
+
+bandit1@bandit:~$ cat < -
+CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
