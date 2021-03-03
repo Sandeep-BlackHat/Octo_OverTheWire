0c6a438def6ba7e99444114d008af90d4d24a0d4
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-04 03:04:00 +0530

    Update Level00.sh

--- Bandit/Level00.sh
+++ Bandit/Level00.sh
@@ -1,1 +1,14 @@
 #Connecting to Bandit using SSH
+
+bandit0@bandit:~$ ls -alps
+total 24
+4 drwxr-xr-x  2 root    root    4096 May  7  2020 ./
+4 drwxr-xr-x 41 root    root    4096 May  7  2020 ../
+4 -rw-r--r--  1 root    root     220 May 15  2017 .bash_logout
+4 -rw-r--r--  1 root    root    3526 May 15  2017 .bashrc
+4 -rw-r--r--  1 root    root     675 May 15  2017 .profile
+4 -rw-r-----  1 bandit1 bandit0   33 May  7  2020 readme
+bandit0@bandit:~$ cat readme
+boJ9jbbUNNfktd78OOpsqOltutMc3MY1
+
+bandit lvl0 - password: boJ9jbbUNNfktd78OOpsqOltutMc3MY1
