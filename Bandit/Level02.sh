59c8507940414ac216403b59e490bcb33c4cf6d3
Ryuzaki Lawliet <55851630+Sandeep-BlackHat@users.noreply.github.com>
2021-03-04 10:19:26 +0530

    Create Level02.sh

---
+++ Bandit/Level02.sh
@@ -0,0 +1,12 @@
+LEVEL 2
+
+bandit2@bandit:~$ ls -alps
+total 24
+4 drwxr-xr-x  2 root    root    4096 May  7  2020 ./
+4 drwxr-xr-x 41 root    root    4096 May  7  2020 ../
+4 -rw-r--r--  1 root    root     220 May 15  2017 .bash_logout
+4 -rw-r--r--  1 root    root    3526 May 15  2017 .bashrc
+4 -rw-r--r--  1 root    root     675 May 15  2017 .profile
+4 -rw-r-----  1 bandit3 bandit2   33 May  7  2020 spaces in this filename
+bandit2@bandit:~$ cat "spaces in this filename"
+UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
